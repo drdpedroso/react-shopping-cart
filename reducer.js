8969a8d3cfff62b2ffa9dec513c06ad8e3d53303
Eduardo Pedroso <drdpedroso@gmail.com>
2016-09-30 18:20:02 -0300

    Add files via upload

---
+++ reducer.js
@@ -0,0 +1,25 @@
+let products = [
+  {name:'esfiha', price:10, qtd: 1, category: 'esfihas'},
+  {name:'esfiha 2', price:20, qtd: 1, category: 'esfihas'},
+  {name:'esfiha 3', price:5, qtd: 1, category: 'sobremesas'},
+  {name:'esfiha 4', price:30, qtd: 1, category: 'esfihas'},
+  {name:'café', price:2, qtd: 1, category: 'bebidas'}
+];
+
+let y = [];
+
+let categories = ['esfihas', 'sobremesas', 'bebidas'];
+
+let reducer = (ac, item) => {
+  if(!ac[item.category]){
+    ac[item.category] = [];
+  } else {
+    let p =  products.find(x => x.category == item.category);
+    ac[item.category].push(p);
+  }
+  return ac;
+}
+
+let res = products.reduce(reducer, y);
+
+console.log(res);
